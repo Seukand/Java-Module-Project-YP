3c10920aa6cd6d544551cbcce92ce0d06ceb975a
Sergei <Seukand.s@mail.ru>
2025-04-27 21:48:59 +0300

    Revert "Решение задачи"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,6 @@
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Race race = new Race();
-
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Введите название автомобиля №" + (i + 1) + ":");
-            String name = scanner.next();
-
-            int speed;
-            while (true) {
-                System.out.println("Введите скорость автомобиля №" + (i + 1) + " (0-250 км/ч):");
-                speed = scanner.nextInt();
-                if (speed >= 0 && speed <= 250) {
-                    break; // Выходим из цикла, если скорость допустима
-                } else {
-                    System.out.println("Некорректная скорость. Повторите ввод.");
-                }
-            }
-
-            Car car = new Car(name, speed);
-            race.determineLeader(car); // Определяем лидера после ввода каждого автомобиля
-        }
-
-        System.out.println("Победитель гонки: " + race.getLeader()); // Выводим победителя
-        scanner.close();
+        System.out.println("Hello world!");
     }
 }
